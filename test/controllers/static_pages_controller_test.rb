41f0d09e412f66d36360f30ced4cb6ac1c231e87
Mirv <reski.raptor@gmail.com>
2015-06-23 18:37:28 +0000

    Fixing title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+@setup = "Ruby on Rails Tutorial Sample App"
+
 
   test "should get home" do
     get :home
@@ -11,12 +13,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@setup}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@setup}"
   end
 end

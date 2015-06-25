8c4cafc367d56ef63e9300eaf6cfe6c3c2db826c
Mirv <reski.raptor@gmail.com>
2015-06-25 12:46:28 +0000

    Testing added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  @setup = "Ruby on Rails Tutorial Sample App"
-
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   test "should get home" do
     get :home
@@ -13,12 +15,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | @setup"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@setup}"
+    assert_select "title", "About | #{@base_title}"
   end
 end

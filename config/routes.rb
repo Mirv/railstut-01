79c9875e920c18fe308a7661bff34eb8e192388a
mirv <reski.raptor@gmail.com>
2015-07-13 12:04:18 +0000

    chap 5.4 - fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
   Rails.application.routes.draw do
+  get 'users/new'
+
     root             'static_pages#home'
     get 'help'    => 'static_pages#help'
     get 'about'   => 'static_pages#about'
     get 'contact' => 'static_pages#contact'
+    get 'signup'  => 'users#new'
   end
 
 

983b1c4b85fde21f2b6636f22dbbe3bef0d5d329
mirv <reski.raptor@gmail.com>
2016-04-20 21:45:44 +0000

    Add password resests & email configuration #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

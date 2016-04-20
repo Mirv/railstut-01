2381f1435db89feef12bc76e92d1d33ae33bd339
mirv <reski.raptor@gmail.com>
2016-04-20 21:24:09 +0000

    Add password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
   Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -13,6 +17,8 @@
     delete 'logout'  => 'sessions#destroy'
     resources  :users
     resources :account_activations, only: [:edit]
+    resources :password_resets,     only: [:new, :create, :edit, :update]
+
   end
 
 
